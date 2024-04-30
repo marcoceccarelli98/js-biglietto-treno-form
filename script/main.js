6b7430794ac5c62a94c1b6804a5c712e9f6d83be
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-04-30 02:06:05 +0200

    [MILESTONE 2]

--- script/main.js
+++ script/main.js
@@ -10,11 +10,21 @@ const saleOver65 = 40;
 
 let userAge = 0;
 let kmToTravel = 0;
+let userName = '';
 let totalPrice = 0;
 
+const elementNameInput = document.getElementById('name-input');
 const elementAge = document.getElementById('age');
 const elementKm = document.getElementById('km');
 const buttonGenerate = document.getElementById('generate');
+const buttonAbort = document.getElementById('abort');
+
+// RESET BORDER FUNCTION
+function resetBorder() {
+    elementNameInput.classList.remove('border-danger');
+    elementKm.classList.remove('border-danger');
+    elementAge.classList.remove('border-danger');
+}
 
 console.log(parseInt(elementKm.value));
 
@@ -30,10 +40,24 @@ buttonGenerate.addEventListener('click', function () {
     const intKm = parseInt(elementKm.value);
     const intAge = parseInt(elementAge.value);
 
+
+    let nameInputOk = (elementNameInput.value !== "");
     let kmInputOk = (!isNaN(intKm) && intKm !== 0);
     let ageInputOk = (!isNaN(intAge) && intAge !== 0);
 
-    if (kmInputOk && ageInputOk) {
+    if (nameInputOk && kmInputOk && ageInputOk) {
+
+        // RESET RED BORDER
+        resetBorder();
+        // elementNameInput.classList.remove('border-danger');
+        // elementKm.classList.remove('border-danger');
+        // elementAge.classList.remove('border-danger');
+
+        let ticketType = 'Biglietto Standard';
+        const cpCode = Math.floor(Math.random() * 99999);
+
+        // - Get value from name input //kmToTravel
+        userName = elementNameInput.value;
 
         // - Get value from age input //userAge
         userAge = Number(elementAge.value);
@@ -49,15 +73,55 @@ buttonGenerate.addEventListener('click', function () {
         //     - 40% for over 65
         if (userAge < 18) {
             totalPrice = totalPrice - (totalPrice * (saleMinor / 100));
+            ticketType = 'Sconto Minori';
         }
         else if (userAge > 65) {
             totalPrice = (totalPrice / 100) * (100 - saleOver65);
+            ticketType = 'Sconto Over 65';
         }
 
+        // TICKET VISUALIZATION
+        const ticketTitle = document.getElementById('ticket-title');
+        const ticket = document.getElementById('ticket');
 
-        console.log(Math.round(totalPrice * 100) / 100 + ' €');
+        ticketTitle.classList.remove('d-none');
+        ticket.classList.remove('d-none');
+
+        document.getElementById('name').innerHTML = userName;
+        document.getElementById('offer').innerHTML = ticketType;
+        document.getElementById('cp').innerHTML = cpCode;
+        document.getElementById('totalprice').innerHTML = (Math.round(totalPrice * 100) / 100 + ' €');
 
 
+    } else { // ERROR MESSAGE
+
+        // RESET RED BORDER
+        resetBorder();
+
+        if (!nameInputOk) {
+            elementNameInput.classList.add('border-danger');
+        }
+        if (!kmInputOk) {
+            elementKm.classList.add('border-danger');
+        }
+        if (!ageInputOk) {
+            elementAge.classList.add('border-danger');
+        }
+        console.log(parseInt(elementKm.value));
     }
+
 });
 
+// ----------------
+// BUTTON ABORT
+// ----------------
+buttonAbort.addEventListener('click', function () {
+    elementNameInput.value = '';
+    elementKm.value = '';
+    elementAge.value = '';
+});
+
+// OUTPUT
+document.getElementById('pricekm').innerHTML = priceKm;
+document.getElementById('minorsale').innerHTML = saleMinor;
+document.getElementById('over65sale').innerHTML = saleOver65;
